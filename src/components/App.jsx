083a2e5ff8bc62a2d894487617cf2b2e0f5bd9e0
eduard-ops <epustynnik@gmail.com>
2022-05-31 12:13:56 +0300

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,71 @@
-export const App = () => {
+import FeedbackOptions from './FeedbackOptions/';
+
+import Statistics from './Statistics/Statistics';
+
+import Notification from './Notification/';
+
+import Section from './Section/';
+
+import Container from './Container/';
+
+import { useState } from 'react';
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = e => {
+    const cur = e.currentTarget.textContent;
+    switch (cur) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const totalSum = () => {
+    return bad + good + neutral;
+  };
+
+  const percentCalc = () => {
+    let num = 0;
+    if (good === 0) return;
+    num += (good * 100) / totalSum();
+    return Math.round(num) + '%';
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Container className="main-container">
+      <Section title={'Plese leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'bad', 'neutral']}
+          onLeaveFeedback={handleClick}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {totalSum() ? (
+          <Statistics
+            bad={bad}
+            good={good}
+            neutral={neutral}
+            total={totalSum()}
+            percent={percentCalc()}
+          />
+        ) : (
+          <Notification message={'There is no feedback'} />
+        )}
+      </Section>
+    </Container>
   );
-};
+}
+
+export default App;
